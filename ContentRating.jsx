8da885ebad18d79b1154ff08a9983f5475c055b9
allencpu <allenproof@yahoo.com>
2024-09-26 02:58:39 -0700

    Update ContentRating.jsx
    Adding third button to Content Rating

--- ContentRating.jsx
+++ ContentRating.jsx
@@ -1,54 +1,54 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
-    this.setState = {
+    this.state = {
         likes: 0,
         dislikes: 0,
         dontcares: 0,
-    handleLike:() => {
+      handleLike:() => {
         this.setState((prevState) => ({
-            likes : prevState.likes + 1
-        }));
-    },
-    handleDontCare: () => {
+            likes: prevState.likes + 1
+          }));
+      },
+      handleDislike:() => {
         this.setState((prevState) => ({
-            dontcares : prevState.dontcares + 1
-        }));
-    },
-    handleDislike: () => {
+            dislikes: prevState.dislikes + 1
+          }));
+      },
+      handleDontCare:() => {
         this.setState((prevState) => ({
-            dislikes : prevState.dislikes + 1
-        }));
-     }
-    };
-  }
+            dontcares: prevState.dontcares + 1
+          }));
+        }
+      };
+    }
+  
   render() {
     return (
      <>
-     <h1> Content Analysis</h1>
+     <h1>Text Content Rating</h1>
      <div className='content-rating'>
-    <p>
-    Please Tell Us What You Think.
-    </p>
-    <div className='rating-buttons'>
-    <button className="like-button" onClick={this.state.handleLike}>
-        Like ({this.state.likes})
-    </button>
-    <button className="dontcare-button" onClick={this.state.handleDontCare}>
-        Don'tCare({this.state.dontcares})
-    </button>
-    <button className="dislike-button" onClick={this.state.handleDislike}>
-        Dislike ({this.state.dislikes})
-    </button>
-</div>
-</div>
+        <p>Text</p>
+        <div className='rating-buttons'>
+        <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dontcare-button" onClick={this.state.handleDontCare}>
+            DontCare ({this.state.dontcares})
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+
+     </div>
      </>
     );
   }
 }
+
 
 export default ContentRating;
